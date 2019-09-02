2077b10244fd6c5406c056edd6bc223a568ffe56
Bao-Linh Le Mai <mlblinh@apcs.vn>
2019-09-02 22:31:20 +0700

    Scene Management

---
+++ Assets/SceneManagerHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void changeScene(bool next){
+	if(next){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}	
+   }
+}
